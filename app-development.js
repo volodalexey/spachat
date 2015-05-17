07dc179d3d5b0a42e03f2d5b45296e8d8d4a6054
Alexey Volodko <alexey@ego-cms.com>
2015-05-17 22:33:09 +0300

    Added websocket into express

--- app-development.js
+++ app-development.js
@@ -4,13 +4,21 @@ var express = require('express')
     , dirPath = '/development'
     , path = require('path')
     , fullPath = path.join(__dirname, dirPath)
-    , fs = require('fs');
+    , fs = require('fs')
+    , expressWs = require('express-ws')(expressApp);
 
 var staticOptions = {
     'index': ['index.html']
 };
 
 expressApp.use(express.static(fullPath, staticOptions));
+
+expressApp.ws('/', function(ws, req) {
+    ws.on('message', function(msg) {
+        console.log(msg);
+    });
+    console.log('socket', req);
+});
 
 expressApp.listen(port);
 console.log('Listening on port: ', port);
