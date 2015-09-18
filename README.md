8df47183d44a918781aaa760922ef698d74a4a35
volodalexey <volodalexey@gmail.com>
2015-09-18 23:16:38 +0300

    Added make production with gulp

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## SPACHAT - Single Page Application Chat
 
 SPACHAT - web application for communication between browsers with the help of WebRTC technology. NodeJS works as signalling server for peers (for exchange session description protocols). Each client uses indexedDB as main storage. Clients can create/join chats, create/join users. Each chat can save its state.
+
+### Make production files
+```bash
+node node_modules/gulp/bin/gulp.js production
+```
---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var htmlreplace = require('gulp-html-replace');
+var lessToCSS = require('gulp-less');
+var rename = require("gulp-rename");
+var rjs = require('gulp-requirejs');
+
+// node node_modules/gulp/bin/gulp.js production
+gulp.task('production', function() {
+    var all_js_name = 'all.js';
+    var all_css_name = 'all.css';
+
+    var config = {};
+    config.mainConfigFile = 'development/js/app/main.js';
+    config.out = all_js_name;
+    config.name = 'app/main';
+    config.include = ['lib/almond.js'];
+    config.findNestedDependencies = true;
+    config.baseUrl = 'development/js/';
+
+    rjs(config)
+        .pipe(gulp.dest('./production/'));
+
+    gulp.src('development/less/total.less')
+        .pipe(lessToCSS())
+        .pipe(rename(all_css_name))
+        .pipe(gulp.dest('./production/'));
+
+    gulp.src('development/index.html')
+        .pipe(htmlreplace({
+            'less-to-all-css': {
+                src: [['' + all_css_name, Date.now()]],
+                tpl: '<link type="text/css" rel="stylesheet" href="%s?%s" />'
+            },
+            'all-js': {
+                src: [['' + all_js_name, Date.now()]],
+                tpl: '<script async defer src="%s?%s"></script>'
+            }
+        }))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('production/'));
+});
