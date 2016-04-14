b91b2c1630d9a0e76834d035e0e75186efce0d87
volodalexey <volodalexey@gmail.com>
2016-04-14 22:44:58 +0300

    Refactored development server

---
+++ react_js/development_server.js
@@ -0,0 +1,109 @@
+var fs = require('fs'),
+  path = require('path'),
+  httpServer = require('http'),
+  server = httpServer.createServer(),
+  port = 8081,
+  index_file_path = 'index.html',
+  public_path = './__build__/',
+  rootFilePath = path.join(__dirname, public_path, index_file_path),
+  webpack = require('webpack'),
+  webpackDevMiddleware = require('webpack-dev-middleware'),
+  webpack_config = require('./webpack.config'),
+  id_Generator = require('./server_js/id_generator'),
+  WebSocketServer = require('ws').Server,
+  webSocketServer = new WebSocketServer({server: server}),
+  web_socket_connections_collection = require('./server_js/web_socket_connections_collection'),
+  clients = [],
+  reqResMiddleware = webpackDevMiddleware(webpack(webpack_config), {
+    publicPath: '/__build__/',
+    stats: {
+      colors: true
+    }
+  });
+
+webSocketServer.on('connection', function(ws) {
+    clients.push(ws);
+    console.log('новое соединение');
+    web_socket_connections_collection.on_wsc_open(ws);
+    ws.on('message', function(messageData) {
+      web_socket_connections_collection.on_wsc_message(this, messageData);
+    });
+    ws.on('close', function(event) {
+      clients.splice(clients.indexOf(ws), 1);
+      web_socket_connections_collection.on_wsc_close(this, event.code, event.message);
+      console.log('Connection close!');
+      if (event.wasClean) {
+        console.log('Соединение закрыто чисто');
+      } else {
+        console.log('Обрыв соединения');
+      }
+      console.log('Код: ' + event.code + ' причина: ' + event.reason);
+    });
+    ws.on('error', function(error) {
+      console.log('Error:' + error.message);
+    });
+  }
+);
+web_socket_connections_collection.apply_wss(clients);
+
+// convert any objects to JSON string format
+var toJSON = function(_object) {
+  return JSON.stringify(_object);
+};
+
+// read async file by provided path
+var readStaticFile = function(options, req, res) {
+
+  fs.stat(options.fullPath, function(err, fileStat) {
+    if (err) {
+      res.writeHead(500, {'Content-Type': 'text/json'});
+      res.end(toJSON(err), 'utf-8');
+      return;
+    }
+
+    if (fileStat.isFile() !== true) {
+      res.writeHead(500, {'Content-Type': 'text/json'});
+      res.end(toJSON({message: 'Is not a file'}), 'utf-8');
+      return;
+    }
+
+    fs.readFile(options.fullPath, options.encoding, function(err, fileContent) {
+      if (err) {
+        res.writeHead(500, {'Content-Type': 'text/json'});
+        res.end(toJSON(err), 'utf-8');
+        return;
+      }
+
+      console.log(options.fullPath, options.encoding, options.contentType, fileStat.size);
+      res.writeHead(200, {'Content-Type': options.contentType, 'Content-Length': fileStat.size});
+      res.end(fileContent, options.encoding);
+    });
+  });
+};
+
+server.on('request', function(req, res) {
+  reqResMiddleware(req, res, function() {
+    var fullPath, extName, contentType, encoding, url = req.url.indexOf('?') >= 0 ? req.url.split('?')[0] : req.url;
+    if (url === '/api/uuid') {
+      res.writeHead(200, {'Content-Type': 'text/json'});
+      res.end(toJSON({uuid: id_Generator.generateId()}), 'utf-8');
+    } else {
+      fullPath = rootFilePath;
+      extName = path.extname(fullPath);
+      switch (extName) {
+        case '.html':
+          contentType = 'text/html';
+          encoding = 'utf-8';
+          break;
+      }
+      readStaticFile({contentType: contentType, encoding: encoding, fullPath: fullPath}, req, res);
+    }
+  });
+});
+server.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Server listening on ' + server.address().port + ', Ctrl+C to stop;');
+});
--- react_js/webpack.config.js
+++ react_js/webpack.config.js
@@ -1,60 +1,61 @@
-'use strict';
-
-var fs = require('fs');
-var path = require('path');
-var webpack = require('webpack');
+var path = require('path'),
+  webpack = require('webpack'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  ExtractPlugin = require('extract-text-webpack-plugin'),
+  production = process.env.NODE_ENV === 'production';
 
 module.exports = {
+  
+  debug: !production,
+  
+  devtool: production ? null : 'inline-source-map',
 
-    devtool: 'inline-source-map',
+  watch: !production,
+  watcherOptions: {
+    aggregateTimeout: 100
+  },
 
-    entry: "./js/main",
-    output: {
-        path: __dirname + '/__build__',
-        filename: 'build.js',
-        chunkFilename: '[id].chunk.js',
-        publicPath: '/__build__/'
-    },
+  entry: {
+    app: './js',
+    vendor: ['react', 'react-dom', 'react-router', 'history']
+  },
+  
+  output: {
+    path: path.join(__dirname, '/__build__'),
+    filename: production ? 'bundle.js?[chunkhash]' : 'bundle.js',
+    publicPath: '/__build__/'
+  },
 
-    watch: true,
-
-    watcherOptions: {
-        aggregateTimeout: 100
-    },
-
-    resolve: {
-        modulesDirectories: ['node_modules'],
-        extension: ['', '.js', '.jsx']
-    },
-
-    resolveLoader: {
-        modulesDirectories: ['node_modules'],
-        moduleTemplates: ['*-loader', '*'],
-        extension: ['', '.js', '.jsx']
-    },
-
-    module: {
-        loaders: [
-            {test: /\.css$/, loader: 'style-loader!css-loader'},
-            {
-                test: /\.less$/,
-                loader: 'style!css!less'
-            },
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            }
-        ]
-    },
-
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin('shared.js'),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-        })
+  module: {
+    loaders: [
+      {
+        test: /\.css$/, 
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.less$/,
+        loader: 'style!css!less'
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
     ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      favicon: './favicon.ico',
+      template: './index.ejs',
+      inject: 'body'
+    }),
+    new ExtractPlugin(production ? 'bundle.css?[chunkhash]' : 'bundle.css'),
+    new webpack.optimize.CommonsChunkPlugin("vendor", production ? "vendor.js?[chunkhash]" : "vendor.js", Infinity),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
 };
