9c9bcd6f1b45008c2af8f6f744ea7b7db0552cc1
volodalexey <volodalexey@gmail.com>
2016-04-15 00:10:04 +0300

    Corrected react dom

--- react_js/js/index.js
+++ react_js/js/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { ReactDOM } from 'react-dom'
+import ReactDOM from 'react-dom'
 import Less from '../less/total.less'
 
 import Index from '../components/index'
 
-ReactDOM((
+ReactDOM.render((
   <Index />
 ), document.getElementById('app'));
 
